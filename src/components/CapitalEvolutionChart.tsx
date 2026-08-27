d95137937f3b0a22b52e1fd499ed9ceba1f0b6ca
Dani Aziz <dany1002008@gmail.com>
2026-08-27 22:50:54 +0200

    Revert chart margin/axis width adjustment

--- src/components/CapitalEvolutionChart.tsx
+++ src/components/CapitalEvolutionChart.tsx
@@ -73,10 +73,10 @@ export default function CapitalEvolutionChart({ points }: { points: HistoryPoint
       </div>
 
       <ResponsiveContainer width="100%" height={320}>
-        <ComposedChart data={chartData} margin={{ top: 10, right: 10, left: -20, bottom: 0 }}>
+        <ComposedChart data={chartData} margin={{ top: 10, right: 10, left: 0, bottom: 0 }}>
           <CartesianGrid strokeDasharray="3 3" stroke="#f4f4f5" />
           <XAxis dataKey="date" tick={{ fontSize: 12 }} tickMargin={8} />
-          <YAxis tick={{ fontSize: 12 }} tickFormatter={formatCompact} width={45} />
+          <YAxis tick={{ fontSize: 12 }} tickFormatter={formatCompact} width={70} />
           <Tooltip
             formatter={(value) =>
               currencyFormatter.format(typeof value === "number" ? value : Number(value))
